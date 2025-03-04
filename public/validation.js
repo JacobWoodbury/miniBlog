52104795840ea89629932b2aaa760879ecf68d64
tim <timmywilliams4665@gmail.com>
2025-03-04 15:17:51 -0800

    did title

--- public/validation.js
+++ public/validation.js
@@ -1,13 +1,30 @@
+alert("Connected");
 
 document.getElementById('post-form').onsubmit = () => {
+    clearErrors();
     let author = document.getElementById('author').value.trim();
     let isValid = true;
+    let title = document.getElementById('title').value.trim();
 
-    if(author === "" || /\d/.test(author)) {
+    if(author === "" || author.isInteger === true) {
         document.getElementById('err-author').style.display = "block";
+        isValid = false;
+    }
+    if(title === "") {
+        document.getElementById('err-title').style.display = "block";
         isValid = false;
     }
 
 
     return isValid;
+}
+
+
+function clearErrors()
+{
+    let errors = document.getElementsByClassName("error");
+    for(let i = 0; i < errors.length; i++)
+    {
+        errors[i].style.display = "none";
+    }
 }
