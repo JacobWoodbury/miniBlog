6850d10c3d06b3b1dd7cf04fcd332dce7ecd74bb
Shawn Nguru <nguru.shawn@student.greenriver.edu>
2025-03-04 14:57:18 -0800

    validation implementation

--- public/validation.js
+++ public/validation.js
@@ -1,5 +1,14 @@
 alert("Connected");
 
-document.getElementById('').onsubmit = () => {
+document.getElementById('post-form').onsubmit = () => {
+    let author = document.getElementById('author').value.trim();
+    let isValid = true;
 
+    if(author === "" || author.isInteger === true) {
+        document.getElementById('err-author').style.display = "block";
+        isValid = false;
+    }
+
+
+    return isValid;
 }
