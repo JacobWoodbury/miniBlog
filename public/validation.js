77752f88829a3b37fb5b279322430066e7f365da
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-03-04 15:06:44 -0800

    updating name validation

--- public/validation.js
+++ public/validation.js
@@ -1,10 +1,9 @@
-alert("Connected");
 
 document.getElementById('post-form').onsubmit = () => {
     let author = document.getElementById('author').value.trim();
     let isValid = true;
 
-    if(author === "" || author.isInteger === true) {
+    if(author === "" || /\d/.test(author)) {
         document.getElementById('err-author').style.display = "block";
         isValid = false;
     }
